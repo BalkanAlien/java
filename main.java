744b51f8385c1087009888021cf329784d94c698
BalkanAlien <67928583+BalkanAlien@users.noreply.github.com>
2020-07-08 00:07:14 +0200

    first-project

---
+++ main.java
@@ -0,0 +1,8 @@
+package App;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("My name is Marija.");
+        System.out.print("This is fun.");
+        
+    }
+}
