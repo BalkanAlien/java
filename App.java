16886a2568b1256aa7e64d6fdb7c4d63024e3208
BalkanAlien <67928583+BalkanAlien@users.noreply.github.com>
2020-07-11 14:14:14 +0200

    loops
    for, while, do-while

---
+++ App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //for loop
+        for (int i = 0; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+        //while loop
+        int a = 0;
+        while (a <= 10)
+        {
+            System.out.println(a);
+            a++;
+        }
+        //do-while
+        int b = 0;
+        do
+        {
+            System.out.println(b);
+            b++;
+            
+        }   while (b <= 10);
+
+    }
+}
