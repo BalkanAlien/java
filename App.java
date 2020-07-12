a2225fe02da32fe7d8e693367f40fb00ea27cc73
BalkanAlien <67928583+BalkanAlien@users.noreply.github.com>
2020-07-13 00:35:37 +0200

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        newclass object = new newclass();
+        object.Greetings();
+        object.simpleMessage();
+        object.Legal(19);
+        object.Legal(3);
+        object.Goodbye();
+
+
+    }
+}
---
+++ newclass.java
@@ -0,0 +1,22 @@
+public class newclass {
+    
+    public void simpleMessage(){
+        System.out.println("This is another class.");
+    }
+
+    public void Greetings(){
+        System.out.println("Hello, Marija");
+    }
+
+    public void Legal(int age){
+        if (age > 17) {
+            System.out.println("Cheers!");
+        }
+        else {
+            System.out.println("You're not allowed to drink yet.");
+        }
+    }
+    public void Goodbye(){
+        System.out.println("See you soon!");
+    }
+}
