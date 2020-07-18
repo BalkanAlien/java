8ee478ac18e7416050cdfa0ed5a12312eda49254
BalkanAlien <67928583+BalkanAlien@users.noreply.github.com>
2020-07-18 19:31:57 +0200

    more convenient ways to use conditions in java

---
+++ conditional-statements.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int age = 21;
+        //System.out.println(age > 50 ? "You are old" : "You are young"); //very convenient way to use if else logical operatorss
+        System.out.println(age > 50 ? "You are old" : age < 30 ? "You are very young" : "You are young"); //nested if statement
+
+
+    }
+}
