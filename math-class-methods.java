2bcdd1cf6c64fccd013fe4f66d5602c62f2217d5
BalkanAlien <67928583+BalkanAlien@users.noreply.github.com>
2020-07-21 00:14:52 +0200

    Add files via upload

---
+++ math-class-methods.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        System.out.println(Math.abs(-26.7)); //absolute value method
+        System.out.println(Math.ceil(24.7)); //method used for rounding numbers UP
+        System.out.println(Math.floor(25.9)); //method used for rounding numbers DOWN
+        System.out.println(Math.max(-25.7, -9.3)); //finds maximum of two numbers
+        System.out.println(Math.min(-25.7, -9.3)); //finds minimum of two numbers
+        System.out.println(Math.pow(2.0, 3.0)); //calculates power of a number
+        System.out.println(Math.sqrt(81)); //calculates square root of a number
+        System.out.println(Math.random()); //prints random number between 0 and 1
+    }
+}
