2d4b55e65152db63f222d74455a2fc75870fb56b
BalkanAlien <67928583+BalkanAlien@users.noreply.github.com>
2020-07-20 23:45:38 +0200

    Add files via upload

---
+++ do-while.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int counter = 15;
+
+        do {
+            System.out.println(counter);
+            counter++;
+        } while (counter <= 10);
+
+    }
+}
